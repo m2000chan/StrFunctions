04728d7f8475187ac59bbffab7c0bfb972ee516a
Max Chan <mchan9@stuy.edu>
2017-10-02 11:01:36 -0400

    update strncpy

--- StrFunctions.c
+++ StrFunctions.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char * strncpy( char *dest, char *source, int n){
+char * mystrncpy( char *dest, char *source, int n){
+    while(n + 1){
+        dest[n] = source[n];
+        n--;
+    }
 
-  for (n; n < strlen(c
-  
+    return dest;
+}
+
+int main(){
+    
+    printf("Testing mystrncpy:\n\n");
+    char a[] = "Hello";
+    char b[] = "Hi";
+    char c[10];
+
+    printf("Creating strings a, b, c:\n\n\ta: %s\n\tb: %s\n\tc: %s\n\n", a, b, c);
+    printf("Running mystrncpy(b, a, 3)...\n\n");
+    mystrncpy(b, a, 1);
+    printf("Result:\n\n\ta: %s\n\tb: %s\n\tc: %s\n", a, b, c);
 }
